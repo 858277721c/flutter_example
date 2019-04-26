c51e016ecd284f492e21a2dfc5cae6b6ed204921
zhengjun <565061763@qq.com>
2019-04-26 21:53:31 +0800

    FBContainer

--- lib/view/container_builder.dart
+++ lib/view/container_builder.dart
@@ -45,20 +45,33 @@ class FBContainer extends FChildWidgetBuilder {
     return copy;
   }
 
-  Container build() {
+  Container build({
+    Key key,
+    AlignmentGeometry alignment,
+    EdgeInsetsGeometry padding,
+    Color color,
+    Decoration decoration,
+    Decoration foregroundDecoration,
+    double width,
+    double height,
+    BoxConstraints constraints,
+    EdgeInsetsGeometry margin,
+    Matrix4 transform,
+    Widget child,
+  }) {
     return Container(
-      key: this.key,
-      alignment: this.alignment,
-      padding: this.padding,
-      color: this.color,
-      decoration: this.decoration,
-      foregroundDecoration: this.foregroundDecoration,
-      width: this.width,
-      height: this.height,
-      constraints: this.constraints,
-      margin: this.margin,
-      transform: this.transform,
-      child: this.child,
+      key: key ?? this.key,
+      alignment: alignment ?? this.alignment,
+      padding: padding ?? this.padding,
+      color: color ?? this.color,
+      decoration: decoration ?? this.decoration,
+      foregroundDecoration: foregroundDecoration ?? this.foregroundDecoration,
+      width: width ?? this.width,
+      height: height ?? this.height,
+      constraints: constraints ?? this.constraints,
+      margin: margin ?? this.margin,
+      transform: transform ?? this.transform,
+      child: child ?? this.child,
     );
   }
 }
