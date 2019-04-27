a07893c6b71f323d691e81db0eb36951933b827e
zhengjun <565061763@qq.com>
2019-04-27 18:46:33 +0800

    dialog

--- lib/dialog/dialog_action_view.dart
+++ lib/dialog/dialog_action_view.dart
@@ -6,13 +6,16 @@ class FDialogAction extends StatelessWidget {
   final TextStyle textStyle;
 
   final AlignmentGeometry alignment;
+  final double height;
 
   FDialogAction({
     this.child,
     this.onPressed,
     this.textStyle,
-    this.alignment,
-  });
+    AlignmentGeometry alignment,
+    double height,
+  })  : this.alignment = alignment ?? Alignment.center,
+        this.height = height ?? 36;
 
   factory FDialogAction.simple(
     String text, {
@@ -29,17 +32,21 @@ class FDialogAction extends StatelessWidget {
     final ThemeData theme = Theme.of(context);
     final DialogTheme dialogTheme = DialogTheme.of(context);
 
+    final TextStyle targetTextStyle = textStyle ??
+        dialogTheme.contentTextStyle ??
+        theme.textTheme.subhead ??
+        TextStyle(
+          fontSize: 13,
+          color: Color(0xFF666666),
+        );
+
     return Container(
+      height: height,
+      alignment: alignment,
       child: InkWell(
         onTap: onPressed,
         child: DefaultTextStyle(
-          style: textStyle ??
-              dialogTheme.contentTextStyle ??
-              theme.textTheme.subhead ??
-              TextStyle(
-                fontSize: 13,
-                color: Color(0xFF666666),
-              ),
+          style: targetTextStyle,
           child: child,
         ),
       ),
